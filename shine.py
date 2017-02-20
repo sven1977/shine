53f76f06279a36e899d64b9178a7474bd7b55104
sven1977 <svenmika1977@gmail.com>
2017-02-20 16:49:24 +0100

    - Finished pre-learning shine-server prototype testing with simple maze runner - TODO: add javascript code for q-table and policy to client - TODO: implement experience feed to shine-server

--- shine.py
+++ shine.py
@@ -9,7 +9,7 @@
 
 import numpy as np
 import random
-from typing import List, Tuple, Union
+from typing import List, Callable
 from abc import ABCMeta, abstractmethod
 
 # import tensorflow as tf
@@ -59,13 +59,16 @@ class World(object):
       - state/action space dimension
     """
 
-    # state_dim: the dimension of the state space (how many features are there in one state vector?)
-    # action_dim: the dimension of the action space (how many features are there in one action vector?)
-    #             for tabular Q-learning, 1 is probably the best value
-    def __init__(self, name: str, state_dim: int, action_shape: Tuple[int]=(2,)):
+    # dim_state: the dimension of the state space (how many features are there in one state vector?)
+    # num_actions: the number of possible actions to chose from in any state
+    # max_size_replay: the maximum size of our replay-memory
+    def __init__(self, name: str, dim_state: int, num_actions: int=2, max_size_replay: int=0):
         self.name = name
-        self.stateDim = state_dim
-        self.actionShape = action_shape
+        self.dimStateDim = dim_state
+        self.numActions = num_actions
+
+        # setup our own replay memory
+        self.replayMemory = ReplayMemory(max_size_replay)
 
 
 class QTable(dict):
@@ -75,42 +78,40 @@ class QTable(dict):
     - values are lists of action/q-value tuples
     """
 
-    # action_shape: the shape of the action space.
+    # action_dim: the shape of the action space.
     #               E.g. (6,) means that we have a 1D array of 6 elements that can represent the entire action space
     #               E.g. (1,2) means we need a matrix of 1 row/2 cols to be able to represent the entire action space
-    def __init__(self, action_shape: tuple):
+    def __init__(self, num_actions: tuple):
         super().__init__()
-        self.actionShape = action_shape
+        self.numActions = num_actions
 
     # adds a new state record under s in this dict and sets all action dims to 0
     def add_state_record(self, s: tuple) -> None:
         assert (s not in self)
-        self[s] = np.zeros(self.actionShape)
+        self[s] = np.zeros(self.numActions)
 
     # returns the q value for s and a
     # - also fills in new values if entries cannot be found
-    def get_q_value(self, s: tuple, a: tuple) -> float:
+    def get_q_value(self, s: tuple, a: int) -> float:
+        assert a < self.numActions, "ERROR in get_q_value: no entry for action {:d} found".format(a)
         if s in self:
-            l = self[s]
-            for entry in l:
-                if entry[0] == a:
-                    return entry[1]
-            raise Exception("ERROR in get_q_value: no entry for action %s found" % str(a))
+            return self[s][a]
         else:
             self.add_state_record(s)
             return 0.0
 
-    # returns the argmax(a) value given s
-    def get_argmax_a(self, s: tuple) -> float:
+    # returns the max(a) value (a Q-value, not an action) given s
+    def get_max_a(self, s: tuple) -> float:
         if s in self:
             l = self[s]
-            argmax = float("-inf")
-            for entry in l:
-                if entry[1] > argmax:
-                    argmax = entry[1]
-            return argmax
+            max_q = float("-inf")
+            for i, q in enumerate(l):
+                if q > max_q:
+                    max_q = q
+            return max_q
         else:
             self.add_state_record(s)
+            # all are the same -> return a random action
             return 0.0
 
     # updates an existing entry or creates a new entry given s,a and a q-value
@@ -131,13 +132,26 @@ class Algorithm(object, metaclass=ABCMeta):
     a simple algorithm object handling the learning algos and the distribution of the same over different CPU/GPU units
     """
 
-    def __init__(self):
-        pass
+    # just set a name here
+    # TODO: remove the callback once we have proper event handling/multi processing in place
+    def __init__(self, name: str, callback: Callable[[dict], None]):
+        self.name = name  # the algo's name
+        self.callback = callback  # the callback to call when certain things happen (progress, done learning, q-table sync, etc..)
 
     # runs this algorithm
     # TODO: multiprocessing implementation
     @abstractmethod
-    def run(self):
+    def run(self, world: World) -> None:
+        pass
+
+    # resets all data associated with this algo (but not the algo's current hyper-parameters)
+    @abstractmethod
+    def reset_parameters(self) -> None:
+        pass
+
+    # sets the algos hyper-parameters by keyword
+    @abstractmethod
+    def set_hyperparameters(self, **kwargs) -> None:
         pass
 
 
@@ -146,22 +160,24 @@ class SimpleQLearner(Algorithm):
     a simple (tabular) q-learning algorithm
     """
 
-    # define some learning parameters
-    # learning_rate (alpha): the learning rate for Q backups
-    # gamma: discount factor
-    # max_num_batches: the maximum number of batches to pull from the ReplayMemory
-    # batch_size: the number of experience tuples to pull for each batch from the ReplayMemory
-    # client_sync_frequency: the number of batches after which a sync with the client will happen
-    # world: the world to run/train on
-    # experience_storage: the ExperienceStorage object to use
-    def __init__(self, learning_rate: float=0.01, gamma: float=0.95, max_num_batches: int=10000, batch_size: int=128,
-                 client_sync_frequency: int=50,
-                 world: Union[World, None] = None,
-                 replay_memory: Union[ReplayMemory, None] = None
+    def __init__(self, name: str, callback: Callable[[dict], None],
+                 learning_rate: float=0.01, gamma: float=0.95, max_num_batches: int=10000, batch_size: int=128,
+                 client_sync_frequency: int=50
                  ):
-        super().__init__()
+        """
 
-        # store parameters
+        Args:
+            name (str): the name of the algorithm
+            callback (Callable[dict, None]): a callback to call when we make progress, are done, etc..
+            learning_rate (float): the learning rate for Q backups
+            gamma (float): discount factor
+            max_num_batches (int): the maximum number of batches to pull from the ReplayMemory
+            batch_size (int): the number of experience tuples to pull for each batch from the ReplayMemory
+            client_sync_frequency (int): the number of experience tuples to pull for each batch from the ReplayMemory
+        """
+        super().__init__(name, callback)
+
+        # store hyper-parameters
         self.learningRate = learning_rate
         self.gamma = gamma
         self.maxNumBatches = max_num_batches
@@ -170,40 +186,37 @@ class SimpleQLearner(Algorithm):
 
         # setup our learning tools
         self.qTable = None
-        self.world = None
-        if world is not None:
-            self.set_world(world)
-
-        self.replayMemory = replay_memory
-
-    # sets the world to learn to the given World object
-    # - then resets our Q-Table and resizes it correctly according to the World's action space
-    def set_world(self, world: World) -> None:
-        self.world = world
-        # generate new Q-table (with all slots initialized to 0)
-        self.qTable = QTable(world.actionShape)
 
     # runs the tabular q-learner algo on the given world and experience-storage
     # TODO: multiprocessing support
-    def run(self):
-        assert(self.world, "Cannot run algorithm without World object!")
-        assert(self.replayMemory, "Cannot run algorithm without replayMemory object!")
+    def run(self, world: World) -> None:
+        replay_memory = world.replayMemory
         for batch_i in range(self.maxNumBatches):
             # pull a batch from our ReplayMemory
-            batch = self.replayMemory.get_random_items(self.batchSize)
+            batch = replay_memory.get_random_items(self.batchSize)
             for s, a, r, s_ in batch:
                 q_sa = self.qTable.get_q_value(s, a)
-                new_q_sa = q_sa + self.learningRate * (r + self.gamma * self.qTable.get_argmax_a(s_) - q_sa)
+                argmax_a_ = self.qTable.i(s_)
+                new_q_sa = q_sa + self.learningRate * (r + self.gamma * self.qTable.get_q_value(s_, argmax_a_) - q_sa)
                 self.qTable.upsert_q_value(s, a, new_q_sa)
                 # TODO: collect some stats: e.g. avg reward per step
 
-            # TODO: frequent progress report to client
-
             # sync our table with client every n batches
+            # send some progress report to client
             if batch_i % self.clientSyncFrequency == 0:
-                # TODO: where to send client sync request (up to some project/manager object)?
-                pass
+                self.callback({"event": "progress", "pct": int(100 * batch_i / self.maxNumBatches)})
+                self.callback({"event": "qTableSync", "qTable": self.qTable})
 
-        # TODO: what to do when we are done
+        # TODO: what to do when we are done?
 
+    # clear out our q-table as our parameter deposit
+    def reset_parameters(self) -> None:
+        self.qTable.clear()
+
+    # name of hyper parameters must match class attribute name
+    def set_hyperparameters(self, **kwargs) -> None:
+        for key, value in kwargs.items():
+            # make sure we don't allow fields that are not hyper parameters
+            assert key in ["learningRate", "gamma", "maxNumBatches", "batchSize", "clientSyncFrequency"]
+            setattr(self, key, value)
 
